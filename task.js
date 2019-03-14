a48b8517cf6102e6bd5935f6ed4d1c18f986c00a
Pavel <pavel0408@gmail.com>
2019-03-14 14:37:01 +0300

    Double Cola

--- task.js
+++ task.js
@@ -1,62 +1,28 @@
-// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+// Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on.
 
-// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+// For example, Penny drinks the third can of cola and the queue will look like this:
 
-// It is much easier to understand with an example:
+// Rajesh, Howard, Sheldon, Sheldon, Leonard, Leonard, Penny, Penny
+// Write a program that will return the name of the person who will drink the n-th cola.
 
-// formatDuration(62)    // returns "1 minute and 2 seconds"
-// formatDuration(3662)  // returns "1 hour, 1 minute and 2 seconds"
-// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+// Input
+// The input data consist of an array which contains at least 1 name, and single integer n.
 
-// Note that spaces are important.
+// 1  ≤  n  ≤  10000000000
+// Output / Examples
+// Return the single line — the name of the person who drinks the n-th can of cola. The cans are numbered starting from 1.
 
-// Detailed rules
-// The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
+// whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 1) == "Sheldon"
+// whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52) == "Penny"
+// whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 7230702951) == "Leonard"
 
-// The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
-
-// A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
-
-// Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
-
-// A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
-
-// A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
-
-function formatDuration(seconds) {
-  if (!seconds) {
-    return `now`;
+function whoIsNext(names, r) {
+  let name;
+  for (let i = 0; i < r; i++) {
+    name = names[0];
+    names.push(name);
+    names.push(name);
+    names.shift();
   }
-  const formatOutput = (number, name) => {
-    if (!number) {
-      return ``;
-    }
-    if (number > 1) {
-      name = name + `s`;
-    }
-    return `${number} ${name},`;
-  };
-
-  const SECONDS_IN_YEAR = 365 * 24 * 60 * 60;
-  const SECONDS_IN_DAY = 24 * 60 * 60;
-  const SECONDS_IN_HOUR = 60 * 60;
-  const SECONDS_IN_MINUTE = 60;
-
-  const years = Math.floor(seconds / SECONDS_IN_YEAR);
-  seconds = seconds % SECONDS_IN_YEAR;
-  const days = Math.floor(seconds / SECONDS_IN_DAY);
-  seconds = seconds % SECONDS_IN_DAY;
-  const hours = Math.floor(seconds / SECONDS_IN_HOUR);
-  seconds = seconds % SECONDS_IN_HOUR;
-  const minutes = Math.floor(seconds / SECONDS_IN_MINUTE);
-  seconds = seconds % SECONDS_IN_MINUTE;
-
-  let outArray = `${formatOutput(years, `year`)} ${formatOutput(days, `day`)} ${formatOutput(hours, `hour`)} ${formatOutput(minutes, `minute`)} ${formatOutput(seconds, `second`)}`.trim().split(` `);
-
-  outArray[outArray.length - 1] = outArray[outArray.length - 1].slice(0, -1);
-  if (outArray.length > 3) {
-    outArray[outArray.length - 3] = outArray[outArray.length - 3].slice(0, -1);
-    outArray.splice(outArray.length - 2, 0, `and`);
-  }
-  return outArray.join(` `).trim();
+  return name;
 }
