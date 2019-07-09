858f9fb93eac254b61a95064414be2a8a088c4bd
Pavel <pavel0408@gmail.com>
2019-07-09 14:26:48 +0300

    Find the unique number

--- task.js
+++ task.js
@@ -1,30 +1,18 @@
-// Complete the method so that it formats the words into a single comma separated value. The last word should be separated by the word 'and' instead of a comma. The method takes in an array of strings and returns a single formatted string. Empty string values should be ignored. Empty arrays or null/nil values being passed into the method should result in an empty string being returned.
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
 
-// formatWords(['ninja', 'samurai', 'ronin']) // should return "ninja, samurai and ronin"
-// formatWords(['ninja', '', 'ronin']) // should return "ninja and ronin"
-// formatWords([]) // should return ""
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains more than 3 numbers.
 
-function formatWords(words) {
-  if (!words || !words.length) {
-    return ``;
-  }
+// The tests contain some very huge arrays, so think about performance.
 
-  words = words.filter((word) => {
-    return word !== `` && word !== ` `;
+function findUniq(arr) {
+  let uniq = null;
+  arr.forEach(element => {
+    if (arr.indexOf(element) === arr.lastIndexOf(element)) {
+      uniq = element
+    }
   });
 
-  if (words.length < 2) {
-    return words.join(` `);
-  }
-
-  words.splice(words.length - 1, 0, `and`);
-
-  return words
-    .map((word, i) => {
-      if (word !== `and` && i !== words.length - 1 && i !== words.length - 3) {
-        return word + `,`;
-      }
-      return word;
-    })
-    .join(` `);
+  return uniq;
 }
