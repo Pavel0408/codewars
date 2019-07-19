604f59f04df8a26f9fa27a2f1f12e886b0f406dc
Pavel <pavel0408@gmail.com>
2019-07-19 14:34:00 +0300

    Word Segmentation: MaxMatch

--- task.js
+++ task.js
@@ -1,27 +1,42 @@
-// Write a function which partitions a list of items based on a given predicate.
+// Some languages like Chinese, Japanese, and Thai do not have spaces between words. However, most natural languages processing tasks like part-of-speech tagging require texts that have segmented words. A simple and reasonably effective algorithm to segment a sentence into its component words is called "MaxMatch".
 
-// After the partition function is run, the list should be of the form [ F, F, F, T, T, T ] where the Fs (resp. Ts) are items for which the predicate function returned false (resp. true).
+// MaxMatch
+// MaxMatch starts at the first character of a sentence and tries to find the longest valid word starting from that character. If no word is found, the first character is deemed the longest "word", regardless of its validity. In order to find the rest of the words, MaxMatch is then recursively invoked on all of the remaining characters until no characters remain. A list of all of the words that were found is returned.
 
-// NOTE: the partitioning should be stable; in other words: the ordering of the Fs (resp. Ts) should be preserved relative to each other.
+// So for the string "happyday", "happy" is found because "happyday" is not a valid word, nor is "happyda", nor "happyd". Then, MaxMatch is called on "day", and "day" is found. The output is the list ["happy", "day"] in that order.
 
-// For convenience and utility, the partition function should return the boundary index. In other words: the index of the first T value in items.
+// The Challenge
+// Write maxMatch, which takes a spaceless, lowercased String as input and returns an Array of all the words found, in the order they were found. All valid words are in the Set VALID_WORDS, which only contains around 500 English words.
 
-// For example:
+// Note: This algorithm is simple and operates better on Chinese text, so accept the fact that some words will be segmented wrongly.
 
-// var items = [1, 2, 3, 4, 5, 6];
-// function isEven(n) {return n % 2 == 0}
-// var i = partitionOn(isEven, items);
-// // items should now be [1, 3, 5, 2, 4, 6]
-// // i     should now be 3
+// Happy coding :)
 
-function partitionOn(pred, items) {
-  const trueArr = items.filter(pred);
-  const falseArr = items.filter((it) => !pred(it));
-  const i = falseArr.length;
-  const newArr = falseArr.concat(trueArr);
-  items.forEach((it, j) => {
-    items[j] = newArr[j];
-  });
+const VALID_WORDS = new Set([`good`, `luck`]);
 
-  return i;
+function maxMatch(sentence) {
+  const output = [];
+  function checkWord(word) {
+    return VALID_WORDS.has(word);
+  }
+
+  function sliceWord(word) {
+    let localWord = word;
+    while (localWord.length > 1) {
+      if (checkWord(localWord)) {
+        return localWord;
+      }
+      localWord = localWord.slice(0, -1);
+    }
+
+    return localWord;
+  }
+  while (sentence.length > 0) {
+    const nextWord = sliceWord(sentence);
+    output.push(nextWord);
+
+    sentence = sentence.slice(nextWord.length, sentence.length);
+  }
+
+  return output;
 }
